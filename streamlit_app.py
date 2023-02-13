c8c851e7d8ae6ffd42886b4f38f707eb7ccb8e51
IRANNAVANDAL <125238680+IRANNAVANDAL@users.noreply.github.com>
2023-02-13 11:18:39 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit 
 streamlit.title('my perents new health dinner')
+
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, spinach and Rocket smoothie')
+streamlit.text('heard-boiled free-range Egg')
+

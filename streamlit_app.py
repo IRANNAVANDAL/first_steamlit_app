35c5e762af298c27805ee6984414f583d141c91f
IRANNAVANDAL <125238680+IRANNAVANDAL@users.noreply.github.com>
2023-02-21 15:23:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ fruit_selectedstreamlit = streamlit.multiselect("pic some fruits:", list(my_frui
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
      
 
 
